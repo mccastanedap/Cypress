f084ca3892ffcf66ec8bc4def70803e75923eac7
Melissa <mc.castanedap@uniandes.edu.co>
2021-11-06 23:02:27 -0500

    Se agrego todo

---
+++ cypress/integration/todo.spec.js
@@ -0,0 +1,35 @@
+describe("TODOS page", () => {
+  it("should add a new TODO", () => {
+    cy.visit("todo")
+      .get("input")
+      .type("new TODO")
+      .get("button[type=submit]")
+      .click()
+      .get(".list-group > .list-group-item:first > p")
+      .first()
+      .should("contain", "1. new TODO");
+  });
+
+  it("should toogle a previous TODO", () => {
+    cy.visit("/todo")
+      .get("input")
+      .type("new TODO")
+      .get("button[type=submit]")
+      .click()
+      .get(".list-group > .list-group-item:first > p")
+      .first()
+      .click()
+      .should("have.class", "complete");
+  });
+
+  it("should delete a new TODO", () => {
+    cy.visit("todo")
+      .get("input")
+      .type("new TODO")
+      .get("button[type=submit]")
+      .click()
+      .get("#Delete")
+      .click()
+      .should("not.exist");
+  });
+});
